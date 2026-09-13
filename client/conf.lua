4b312916074f6e98f73fd01d34e29653b157cadb
FarhanAhmad54 <farhan056ahmad@gmail.com>
2026-09-13 13:20:40 +0530

    Add LÖVE configuration

---
+++ client/conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.identity = "wall-vertical-race"
+    t.version = "11.5"
+    t.window.title = "Wall Vertical Race"
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.resizable = true
+    t.window.vsync = 1
+    t.window.highdpi = true
+end
